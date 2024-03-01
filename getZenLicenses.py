a4522c518d3c32b49aae5fe3ca4e9824aba67df5
sunkingbms <bms.scripts@sunking.com>
2024-03-01 10:54:23 +0100

    Two files added

---
+++ getZenLicenses.py
@@ -0,0 +1,49 @@
+import requests
+
+def getUsersPerGrp(instance, userMail, group_id, token_id):
+    users_logins = []
+    users = []
+    id = 1
+
+    
+    try:
+        while True:
+            url = f"https://{instance}.zendesk.com/api/v2/groups/{group_id}/users.json?page={id}"
+            res = requests.get(url, auth=(userMail, token_id))
+            data = res.json()
+            users.extend(data['users'])
+
+            if not data.get('next_page'):
+                break
+
+            id += 1
+        
+        print(len(users))
+
+        for user in users:
+            last_login = str(user.get('last_login_at', '')).split("T")[0]
+            name = user.get('name', '')
+            
+            users_logins.append({
+                'name': name,
+                'email': user.get('email', ''),
+                'role': user.get('role', ''),
+                'created_at': str(user.get('created_at', '')).split("T")[0],
+                'last_login': last_login,
+                'Location': user.get('time_zone', '')
+            })
+        
+        
+        sorted_logins = sorted(users_logins, key=lambda x: x.get('name', ''), reverse=False)
+        
+        return sorted_logins
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return []
+
+token_id = 'rBBl1gxdbI8UahEHr01uP8K3dBy0Igl95yXjf3kf'
+userMail = 'ian.kamanda@greenlightplanet.com/token'
+
+logins = getUsersPerGrp('sunkingpaygo', userMail, 24452815, token_id)
+print(logins)
---
+++ getZendeskGrp.py
@@ -0,0 +1,43 @@
+########################### Getting Users names and last login per group ##########################
+
+def getUsersPerGrp(instance, userMail, group_id, token_id):
+    
+    users_logins = []
+    
+    url = f"https://{instance}.zendesk.com/api/v2/groups/{group_id}/users"
+    
+    try:
+        res = requests.get(url, auth=(userMail, token_id))
+        data = res.json()
+        users = data['users']
+
+        for user in users:
+            
+            last_login = str(user['last_login_at']).split("T")[0]
+            name = user['name']
+            
+            users_logins.append({
+                'name': name,
+                "email": user['email'],
+                "role": user['role'],
+                "created_at": user['created_at'].split("T")[0],
+                "last login": last_login,
+                "Location": user["time_zone"]
+            })
+            
+        sorted_logins = sorted(users_logins, key=lambda x: list(x.values())[0], reverse=True)
+        
+        return sorted_logins
+    
+    except requests.exceptions.RequestException as e:
+    
+        print(f"Error: {e}")
+
+
+token_id = 'wqOzDTtFdWiviPfb6BRMbCerCgPU5kFp3IojB8lW'
+
+userMail = 'ian.kamanda@greenlightplanet.com' + '/token'
+
+logins = getUsersPerGrp('sunkingpaygong', userMail, 24452815, token_id)
+
+output = [{'result': logins}]
